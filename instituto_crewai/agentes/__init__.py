0d10421db7811ccf78c975f0da7a8c581187d91d
rodrigo.goncalves <ro.go.calves@gmail.com>
2024-07-05 09:30:55 -0300

    Removendo projeto antigo, não deu bom

--- instituto_crewai/agentes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .planejador import Planejador
-from .pesquisador import Pesquisador
-from .escritor import Escritor
-from .fotografo import Fotografo
-from .gerente import Gerente
--- instituto_crewai/tasks/__init__.py
+++
@@ -1,5 +0,0 @@
-from .plano_task import PlanoTask
-from .pesquisa_task import PesquisaTask
-from .escrita_task import EscritaTask
-from .criacao_imagem_task import CriacaoImagemTask
-from .revisao_task import RevisaoTask
