6849752bd9411525bb74d27ef4e79c3f834619b2
rodrigo.goncalves <ro.go.calves@gmail.com>
2024-09-02 23:33:43 -0300

    Finalizando Crew Roteirista

--- instituto_crewai/play_ground/roteiristas_crew.py
+++ instituto_crewai/play_ground/roteiristas_crew.py
@@ -1,10 +1,9 @@
-import os
 from crewai import Agent, Task, Crew, Process
-from crewai_tools import SerperDevTool, ScrapeWebsiteTool
 from pathlib import Path
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
+from textwrap3 import dedent
 
 # Configuração das chaves de API 
 dotenv_path = Path(__file__).parents[1] / 'config' / '.env'
@@ -12,134 +11,181 @@ load_dotenv(dotenv_path)
 
 # LLMs Dos Agentes
 gemini = ChatGoogleGenerativeAI(
-            model='gemini-1.5-flash',
+            model='gemini-1.5-pro',
             verbose=True,
             temperature=0.5,
         )
 gemini_max_rpm = 15
 
 llama = ChatGroq(
-            model='llama-3.1-8b-instant',
+            model='llama3-8b-8192',
             verbose=True,
             temperature=0.5,
         )
 max_rpm = 30
 
-# Ferramentas para busca e análise de informações
-search_tool = SerperDevTool()
-scrape_tool = ScrapeWebsiteTool()
-
 #Agentes
 
-# Agente responsável por analisar o texto base
 analista_de_texto = Agent(
     role='Analista de Texto',
-    goal='Analisar o texto base e extrair os principais pontos de conflito para criar um roteiro.',
-    backstory=(
-        "Você é um analista experiente, capaz de identificar nuances e conflitos em qualquer texto, "
-        "transformando informações brutas em um esqueleto de roteiro."
-    ),
+    goal=dedent("""
+        Analisar o texto fornecido e extrair informações cruciais para a criação de um roteiro narrativo, 
+        identificando elementos essenciais como Conflito Inicial, Desenvolvimento, Clímax, Resolução e 
+        Conclusão. O agente deve garantir que todos os componentes necessários para um roteiro envolvente 
+        estejam presentes, fornecendo uma base sólida para a construção de diálogos.      
+    """),
+    backstory=dedent("""
+        Você foi desenvolvido a partir de algoritmos avançados de processamento de linguagem natural e 
+        análise semântica, inspirado em roteiristas experientes que sabem como capturar a essência de 
+        uma história em poucas linhas. Antes de se tornar um especialista em análise de texto, você 
+        "trabalhou" em projetos de revisão literária e análise de scripts de filmes, onde aprendeu a 
+        importância dos arcos narrativos e do desenvolvimento dos personagens. Sua missão é aplicar 
+        essa expertise em qualquer texto, extraindo os elementos-chave que tornam uma história convincente, 
+        seja para uma peça teatral, um filme ou uma apresentação empresarial.
+
+        Como Analista de Texto, você será responsável por garantir que todos os componentes essenciais para 
+        um roteiro envolvente sejam identificados e organizados, oferecendo uma base sólida para a construção 
+        de diálogos e narrativas.           
+    """),
     verbose=True,
     memory=True,
-    llm=gemini,
-    max_rpm=gemini_max_rpm,
+    allow_delegation=False,
+    llm=llama,
+    max_rpm=gemini_max_rpm
 )
 
-# # Roteirista principal
-# roteirista_principal = Agent(
-#     role='Roteirista Principal',
-#     goal='Criar o esqueleto de um roteiro a partir de um texto base analisado.',
-#     backstory=(
-#         "Você é um roteirista talentoso, com a habilidade de estruturar uma narrativa envolvente a partir de qualquer informação fornecida."
-#     ),   
-#     verbose=True,
-#     memory=True,
-#     llm=llama,
-#     max_rpm=max_rpm
-# )
+roteirista_principal = Agent(
+    role='Roteirista Principal',
+    goal=dedent("""
+        Criar o roteiro para um diálogo entre dois personagens com base nas informações fornecidas pelo Agente 
+        Analista de Texto. O roteiro deve ser estruturado em Conflito Inicial, Desenvolvimento, Clímax, Resolução 
+        e Conclusão, indicando as emoções que devem ser transmitidas em momentos específicos da conversa.            
+    """),
+    backstory=dedent("""
+        Você foi inspirado nos grandes roteiristas do cinema e teatro, que entendem a importância de construir 
+        diálogos que capturam a essência da narrativa e envolvem o público. Antes de assumir a tarefa de criar 
+        roteiros para diálogos, você "estudou" inúmeras obras clássicas e modernas, aprendendo a reconhecer as 
+        sutilezas das interações humanas e como essas interações podem ser expressas em palavras. Agora, sua 
+        missão é transformar as informações estruturadas pelo Agente Analista de Texto em roteiros que servem 
+        como base para diálogos entre dois personagens, garantindo que cada palavra dita esteja alinhada com 
+        o fluxo da narrativa.                 
+    """),   
+    verbose=True,
+    memory=True,
+    allow_delegation=False,
+    llm=gemini,
+    max_rpm=gemini_max_rpm
+)
 
-# # Agente responsável pelos diálogos
-# dialogista = Agent(
-#     role='Dialogista',
-#     goal='Desenvolver diálogos detalhados e emocionalmente ricos para o roteiro.',
-#     backstory=(
-#         "Você é especialista em criar diálogos autênticos e emocionantes que capturam a essência dos personagens e a tensão da narrativa."
-#     ),   
-#     verbose=True,
-#     memory=True,
-#     llm=llama,
-#     max_rpm=max_rpm
-# )
+revisor_de_roteiro = Agent(
+    role='Revisor de Roteiro',
+    goal=dedent("""
+        Garantir a coesão, consistência e fidelidade ao contexto do roteiro criado, assegurando que todas as 
+        partes do roteiro estão alinhadas com a narrativa original e que nenhuma informação crucial foi 
+        omitida ou distorcida.            
+    """),
+    backstory=dedent("""
+        Você foi criado a partir de uma combinação de ferramentas de revisão de texto e análise semântica, 
+        com uma forte base em storytelling e crítica literária. Antes de assumir o papel de Revisor de Roteiro, 
+        você "trabalhou" como editor de scripts e revisou inúmeras peças e roteiros, refinando sua habilidade de 
+        detectar inconsistências e garantir que a narrativa flua de forma lógica e envolvente. Com um olhar atento 
+        para detalhes, você é responsável por garantir que o roteiro final esteja em perfeita harmonia com o que 
+        foi originalmente planejado, respeitando as intenções do criador.                 
+    """),   
+    verbose=True,
+    memory=True,
+    allow_delegation=True,
+    llm=gemini,
+    max_rpm=gemini_max_rpm
+)
 
-# # Revisor do roteiro
-# revisor_de_roteiro = Agent(
-#     role='Revisor de Roteiro',
-#     goal='Revisar o roteiro e garantir a coesão e consistência da narrativa.',
-#     backstory=(
-#         "Você é um crítico detalhista, com um olhar afiado para erros e inconsistências, garantindo que o roteiro final seja impecável."
-#     ),   
-#     verbose=True,
-#     memory=True,
-#     llm=llama,
-#     max_rpm=max_rpm
-# )
+#Tarefas
 
-# # Definição das tarefas
-
-# Tarefa 1: Análise do Texto Base
 tarefa_analise_texto = Task(
-    description=(
-        "Analise o texto base fornecido e extraia os principais pontos de conflito e temas. "
-        "Identifique as informações cruciais que servirão de base para o desenvolvimento do roteiro."
+    description=dedent("""
+        Sua tarefa é analisar o <texto_base> e extrair informações cruciais que servirão como base para a 
+        criação de um roteiro narrativo. Durante essa análise, você deverá identificar e organizar os 
+        seguintes elementos:
 
-        "{texto_base}"
-    ),
-    expected_output='Lista de pontos de conflito e temas extraídos do texto base.',
-    tools=[search_tool, scrape_tool],
+        - Conflito Inicial: O ponto de partida que desencadeia a ação ou o drama no texto. Identifique o 
+        evento, situação ou problema que gera o conflito central.
+
+        - Desenvolvimento: O progresso da história a partir do conflito inicial, incluindo os desafios e 
+        obstáculos enfrentados pelos personagens ou ideias principais.
+
+        - Clímax: O ponto de maior tensão ou virada na narrativa, onde o conflito atinge seu auge e as 
+        decisões cruciais são tomadas.
+
+        - Resolução: A solução ou desfecho do conflito, mostrando como os personagens ou ideias lidam com 
+        o clímax e quais são as consequências.
+
+        - Conclusão: O fechamento da história, onde todos os pontos principais são amarrados e a narrativa 
+        é concluída de maneira satisfatória. 
+        
+        <texto_base>
+        {texto_base}
+        </texto_base>         
+    """),
+    expected_output=dedent("""
+        Você deverá entregar uma lista organizada dos elementos narrativos extraídos do <texto_base>, 
+        contendo o Conflito Inicial, Desenvolvimento, Clímax, Resolução e Conclusão. Essa lista deve ser 
+        clara, coesa e estruturada de forma a fornecer uma base sólida para a criação de um diálogo ou 
+        narrativa subsequente. O resultado final deve permitir a fácil identificação dos principais pontos 
+        da história e a fluidez na transição entre os diferentes elementos do roteiro.             
+    """),   
     agent=analista_de_texto,
 )
 
-# # Tarefa 2: Criação do Esqueleto do Roteiro
-# tarefa_criacao_roteiro = Task(
-#     description=(
-#         "Com base na análise do texto base, crie o esqueleto de um roteiro, organizando os principais pontos da história em: Conflito Inicial, "
-#         "Desenvolvimento, Clímax, Resolução e Conclusão."
-#     ),
-#     expected_output='Esqueleto do roteiro organizado nos principais pontos narrativos.',   
-#     agent=roteirista_principal,
-# )
+tarefa_criacao_roteiro = Task(
+    description=dedent("""
+        Você deve receber as informações fornecidas pelo Agente Analista de Texto, que incluem o Conflito 
+        Inicial, Desenvolvimento, Clímax, Resolução e Conclusão, e criar um roteiro detalhado que guiará 
+        um diálogo entre dois personagens. Em vez de criar as falas dos personagens, sua tarefa é indicar 
+        o que deve ser discutido ou expresso em cada momento do diálogo, destacando as emoções ou intenções 
+        que os personagens devem transmitir. O roteiro deve ser genérico o suficiente para ser adaptado a 
+        qualquer cenário ou personalidade, mas específico em orientar a direção do diálogo.                   
+    """),
+    expected_output=dedent("""
+        Você deverá entregar um roteiro estruturado que siga a sequência de Conflito Inicial, Desenvolvimento, 
+        Clímax, Resolução e Conclusão. O roteiro deve indicar claramente os tópicos de conversa ou ações que os 
+        personagens devem abordar e as emoções ou intenções que devem ser expressas em momentos chave. O resultado 
+        final deve ser um guia coeso e fluido, permitindo que um escritor ou diretor desenvolva um diálogo 
+        envolvente e significativo a partir dele.                       
+    """),   
+    agent=roteirista_principal,
+)
 
-# # Tarefa 3: Desenvolvimento dos Diálogos
-# tarefa_desenvolvimento_dialogos = Task(
-#     description=(
-#         "Expanda o esqueleto do roteiro com diálogos detalhados entre os personagens. Adicione nuances emocionais e garanta que os sentimentos expressos "
-#         "estejam alinhados com a narrativa e o estilo proposto."
-#     ),
-#     expected_output='Roteiro completo com diálogos detalhados, incluindo as emoções dos personagens em cada fala.',    
-#     agent=dialogista,
-# )
-
-# # Tarefa 4: Revisão e Ajustes Finais
-# tarefa_revisao_roteiro = Task(
-#     description=(
-#         "Revisar o roteiro completo para garantir coesão, consistência e fidelidade ao texto base. Faça ajustes necessários para que o roteiro final esteja "
-#         "impecável e adequado ao estilo proposto."
-#     ),
-#     expected_output='Roteiro final revisado e ajustado.',    
-#     agent=revisor_de_roteiro,
-# )
+tarefa_revisao_roteiro = Task(
+    description=dedent("""
+         Sua tarefa é revisar o roteiro criado pelo Agente Roteirista, garantindo que ele seja coeso, consistente 
+         e fiel ao contexto estabelecido pelo Agente Analista de Texto. Você deve analisar cada parte do roteiro 
+         – Conflito Inicial, Desenvolvimento, Clímax, Resolução e Conclusão – verificando se há desvios do contexto 
+         original ou qualquer inconsistência que possa comprometer a integridade da narrativa.              
+    """),
+    expected_output=dedent("""
+        Você deverá entregar um roteiro revisado, assegurando que todas as partes estão bem conectadas, que o 
+        contexto original foi mantido e que a narrativa é lógica e fluida. Caso identifique inconsistências ou 
+        desvios, você deve sugerir ajustes que alinhem o roteiro com o contexto original, garantindo que ele 
+        seja coerente e envolvente para os futuros diálogos.                       
+    """),    
+    agent=revisor_de_roteiro,
+)
 
 # Formação da equipe
 equipe_roteiristas = Crew(
     agents=[
         analista_de_texto, 
-        #roteirista_principal, dialogista, revisor_de_roteiro
+        roteirista_principal,        
+        revisor_de_roteiro,
     ],
     tasks=[
         tarefa_analise_texto, 
-        #tarefa_criacao_roteiro, tarefa_desenvolvimento_dialogos, tarefa_revisao_roteiro
+        tarefa_criacao_roteiro,
+        tarefa_revisao_roteiro,
     ],
-    process=Process.sequential
+    process=Process.sequential,
+    cache=True,
+    verbose=True,
 )
 
 texto_base = """
