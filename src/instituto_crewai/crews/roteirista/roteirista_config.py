a2b82de04edd52746a7d0b4adefcba5beb720b8d
rodrigo.goncalves <ro.go.calves@gmail.com>
2024-09-17 15:02:51 -0300

    Criado a configuração padrão para a Equipe Roteirista

---
+++ src/instituto_crewai/crews/roteirista/roteirista_config.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any
+from agente_config import AgenteConfig
+from models import GroqManager, LLMA3_8
+
+def default_config() -> Dict[str, Any]:
+    """
+    Função que retorna a configuração padrão para o roteirista.
+
+    Retorna:
+        Dict[str, Any]: Um dicionário contendo 'max_rpm' e a lista 'agente_config'.
+    """
+    llm_manager = GroqManager(temperature=0.75, model_key=LLMA3_8)
+    llm_instance = llm_manager.get_instance()
+    
+    return {
+        'max_rpm': llm_manager.get_model_rpm(),
+        'agente_config': [AgenteConfig(llm=llm_instance) for _ in range(3)]
+    }
