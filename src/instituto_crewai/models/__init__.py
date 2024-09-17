b1983c6a0082739e98a59d581a7df7479aa64f06
rodrigo.goncalves <ro.go.calves@gmail.com>
2024-09-17 14:52:59 -0300

    Criando import do pacote models

--- src/instituto_crewai/models/__init__.py
+++ src/instituto_crewai/models/__init__.py
@@ -1,1 +1,24 @@
-from .gemini_manager import Gemini
+from .gemini_manager import GeminiManager
+from .groq_manager import GroqManager
+
+from .groq_manager import (
+    GEMMA_1_7,
+    GEMMA_2_9,
+    LLMA3_1_70,
+    LLMA3_1_8,
+    LLMA3_70,
+    LLMA3_8,
+    MIXTRAL_8_7,
+)
+
+__all__ = [
+    "GeminiManager",
+    "GroqManager",
+    "GEMMA_1_7",
+    "GEMMA_2_9",
+    "LLMA3_1_70",
+    "LLMA3_1_8",
+    "LLMA3_70",
+    "LLMA3_8",
+    "MIXTRAL_8_7",
+]
